9c94e31e1f206b13254a98ee7bc9289a3ba52bd4
AmtTawsik <amttawsik.cse@gmail.com>
2023-11-29 15:14:39 +0600

    Some EDIt

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,6 +9,9 @@ export default {
   // default_student_password: process.env.DEFAULT_STUDENT_PASSWORD,
   // default_faculty_password: process.env.DEFAULT_FACULY_PASSWORD,
   // default_admin_password: process.env.DEFAULT_ADMIN_PASSWORD,
+  // default_student_password: process.env.DEFAULT_STUDENT_PASSWORD,
+  // default_faculty_password: process.env.DEFAULT_FACULY_PASSWORD,
+  // default_admin_password: process.env.DEFAULT_ADMIN_PASSWORD,
   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
   jwt: {
     secret: process.env.JWT_SECRET,
